2fa665d8d02a0a9aa4268c47125fe5a1e08a0410
Daqiang <daqiang@Guichengs-MacBook-Air.local>
2019-09-14 16:17:30 -0700

    setup board

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,9 @@
+import java.util.Random;
 public class Board{
 
     //constructor
-    public void Board[]{
+    public void Board {
+        Random rand = new Random();
         int [][] boardMat= {
                 {1, 1 , 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
                 {1, 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -17,6 +19,35 @@ public class Board{
                 {1, 1 , 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
         };
         /* nothing = 0, fence = 1, moh = 2, player = 3*/
-        3
+        int fenceTracker = 0;
+        int mohsTracker = 0;
+        int playerTracker = 0;
+        int RowNum;
+        int ColNum;
+        //making fences
+        while(fenceTracker<20){
+            RowNum = rand.nextInt(9)+1;
+            ColNum = rand.nextInt(9)+1;
+            if(boardMat[RowNum][ColNum] == 0)(
+                boardMat[RowNum][ColNum] = 1;
+                fenceTracker++;)
+        }
+        //making mohs
+        while (mohsTracker<12){
+            RowNum = rand.nextInt(9)+1;
+            ColNum = rand.nextInt(9)+1;
+            if(boardMat[RowNum][ColNum] == 0){
+                boardMat[RowNum][ColNum] = 2;
+                mohsTracker++;}
+        }
+        //making player
+        while(playerTracker<1){
+            RowNum = rand.nextInt(9)+1;
+            ColNum = rand.nextInt(9)+1;
+            if(boardMat[RowNum][ColNum] == 0){
+                boardMat[RowNum][ColNum] = 3;
+                playerTracker++;}
+            }
+        }
+    //methods
     }
-}
