cce5ae09ae9e30010a7124ee81102f614b18205f
Daqiang <daqiang@Guichengs-MacBook-Air.local>
2019-09-15 16:58:51 -0700

    worked on Mhos stuff

--- src/Mhos.java
+++ src/Mhos.java
@@ -1,9 +1,29 @@
 //Use the class as a group of method
 public class Mhos {
     //contructor not needed?
+    public void Mhos(){
+        int[] mhosX = new int{12};
+        int[] mhosY = new int{12};
+        int counter = 0;
+        //sets up the mhos location into two arrays
+        for(int i =0; i<boardMat.legnth; i++)
+            for(int j=0; j<boardMat[i].legnth; j++)
+                if(boardMat[i][j] == 2)
+                    mhosX[counter] = i;
+                    mhosY[counter] = j;
+                    counter++;
+    }
     //methods
+    void mhoMove(x, y){
+        int xDiff;
+        int yDiff;
+        //cycles through every mho position
+        for(int i = 0; i<12; i++)
+
+
+    }
+
+    }
+
 }
-//for looping through the matrix to find 2's, mhos, then do the movement stuff(make those methods). MOVE LATER
-for(int i =0; i<boardMat.legnth; i++)
-    for(int j=0; i<boardMat[i]; i++)
-        if(boardMat[i][j] == 2)
+
